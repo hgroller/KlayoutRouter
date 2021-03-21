2ff81dd4affaf1a08fdc38561e9bb97a750f05b5
hgroller <h-groller@swbell.net>
2021-03-21 18:52:53 -0500

    Add files via upload
    Fixed Menu Definition

--- Router.py
+++ Router.py
@@ -37,36 +37,39 @@ if cell == None:
   raise Exception("No cell")
 
 #Get the  Box Border
-texts = 0
-layer_info = LayerInfo.new(13,0)
-layer = layout.layer(layer_info) 
-iter = layout.begin_shapes(cv.cell_index, layer)
-if iter.shape().is_box():
-  ux =  iter.shape().box.right
-  uy =  iter.shape().box.top
+def GetBox():
+  texts = 0
+  layer_info = LayerInfo.new(13,0)
+  layer = layout.layer(layer_info) 
+  iter = layout.begin_shapes(cv.cell_index, layer)
+  if iter.shape().is_box():
+    ux =  iter.shape().box.right
+    uy =  iter.shape().box.top
+  return ux,uy  
 
-#Get the Text
-texts = 0
-layer_info = LayerInfo.new(9,0)
-layer = layout.layer(layer_info) 
-iter = layout.begin_shapes(cv.cell_index, layer)
-coords = []
-strlst = []
-while not iter.at_end():
-  if iter.shape().is_text():
-     string =  iter.shape().text.string
-     x = iter.shape().text.x
-     y = iter.shape().text.y
-     strlst.append(string)
-     lst = [string,x,y]
-     coords.append(lst)
-  iter.next()
+def Get_Text():
+  #Get the Text
+  texts = 0
+  layer_info = LayerInfo.new(9,0)
+  layer = layout.layer(layer_info) 
+  iter = layout.begin_shapes(cv.cell_index, layer)
+  coords = []
+  strlst = []
+  while not iter.at_end():
+    if iter.shape().is_text():
+      string =  iter.shape().text.string
+      x = iter.shape().text.x
+      y = iter.shape().text.y
+      strlst.append(string)
+      lst = [string,x,y]
+      coords.append(lst)
+    iter.next()
 
-srt = sorted(coords)
-#print("The Array is: ", srt)
-
-#Remove Duplicates from text list
-strlst = sorted(list(dict.fromkeys(strlst)))
+  srt = sorted(coords)
+  #print("The Array is: ", srt)
+ #Remove Duplicates from text list
+  strlst = sorted(list(dict.fromkeys(strlst)))
+  return strlst,srt
 
 def coords(path):
     result = []
@@ -88,125 +91,116 @@ def create_box(crd,lay):
 layer_info = LayerInfo.new(9,0)
 layer_index = layout.layer(layer_info) 
 
-#Build Matrix for later
-#Build Maze based on  Border Boundary Box
-#rows = int(input()) ux from Border Box
-#cols = int(input())  uy from Border Box
-
-#rows = round(abs(int(ux))/1000)
-#cols =  round(abs(int(uy))/1000)
-
-#w, h = rows, cols;
-#Matrix = [[0 for x in range(w)] for y in range(h)] 
-#for  s in Matrix:
-    #print(s)
 
 #Filter the list to Search the text list
 #For A* Routing 
 #This works for 2 pins only at the present time
-#All placements of Text needs to be centerd in via 
 #All placements of Text need too be on a int no floats
+def Router():
 
-for search in strlst: 
-  filtered = list(filter(lambda x:x[0]==search,srt))
-  print('The filtered list for is: ' , filtered[0]) 
-  wd = .3
-  lay = 11
-  lc1  = math.ceil(int(filtered[0][1]/1000))
-  lc2  = math.ceil(int(filtered[0][2]/1000))
-  lc3  = math.ceil(int(filtered[1][1]/1000))
-  lc4  = math.ceil(int(filtered[1][2]/1000))
-  loc1 = (lc1,lc2)
-  loc2 = (lc3,lc4)
-  pth  = AstarRouter.main(loc1,loc2)
+  stlst,srt = Get_Text()
+  filtered = None
+  for search in stlst: 
+    filtered = list(filter(lambda x:x[0]==search,srt))
+    #print('The filtered list for is: ' , filtered[0]) 
+    wd = .3
+    lay = 11
+    lc1  = math.ceil(int(filtered[0][1]/1000))
+    lc2  = math.ceil(int(filtered[0][2]/1000))
+    lc3  = math.ceil(int(filtered[1][1]/1000))
+    lc4  = math.ceil(int(filtered[1][2]/1000))
+    loc1 = (lc1,lc2)
+    loc2 = (lc3,lc4)
+    pth  = AstarRouter.main(loc1,loc2)
  
   #Parse in the Vert/Horz Coords
-  vert = [el[0] for el in pth]
-  horz = [el[1] for el in pth]
+    vert = [el[0] for el in pth]
+    horz = [el[1] for el in pth]
 
-  hoz = [k for k, v in OrderedCounter(horz).items() if v > 1]
-  vet = [k for k, v in OrderedCounter(vert).items() if v > 1]
+    hoz = [k for k, v in OrderedCounter(horz).items() if v > 1]
+    vet = [k for k, v in OrderedCounter(vert).items() if v > 1]
 
   #Build Vert/Horz Paths
   #Metal 1
-  lay = 9
-  wd = .3
-  for p in hoz: 
-      filteredh = list(filter(lambda x:x[1]==p,pth))
-      print(filteredh)
-      crd = coords(filteredh)
-      create_path(crd,wd,lay)
+    lay = 9
+    wd = .3
+    for p in hoz: 
+        filteredh = list(filter(lambda x:x[1]==p,pth))
+        print(filteredh)
+        crd = coords(filteredh)
+        create_path(crd,wd,lay)
   
   
-  wd = .3
-  for p in vet: 
-     filteredv = list(filter(lambda x:x[0]==p,pth))
-     print(filteredv)
-     crd = coords(filteredv)
-     lay = 11
-     create_path(crd,wd,lay)
+    wd = .3
+    for p in vet: 
+       filteredv = list(filter(lambda x:x[0]==p,pth))
+       print(filteredv)
+       crd = coords(filteredv)
+       lay = 11
+       create_path(crd,wd,lay)
      #Build Metal1/2 Overlap
-     mwd =  wd / 2
-     vwd = wd / 4
-     x1 =  abs(int(crd[0].x)/1000)
-     y1 =  abs(int(crd[0].y)/1000)
-     x2 =  abs(int(crd[1].x)/1000)
-     y2 =  abs(int(crd[1].y)/1000)
+       mwd =  wd / 2
+       vwd = wd / 4
+       x1 =  abs(int(crd[0].x)/1000)
+       y1 =  abs(int(crd[0].y)/1000)
+       x2 =  abs(int(crd[1].x)/1000)
+       y2 =  abs(int(crd[1].y)/1000)
      #Metals
-     lx1 = x1 - mwd
-     ly1 = y1 - mwd
-     ux1 = x1 + mwd
-     uy1 = y1 + mwd
+       lx1 = x1 - mwd
+       ly1 = y1 - mwd
+       ux1 = x1 + mwd
+       uy1 = y1 + mwd
     
-     lx2 = x2 - mwd
-     ly2 = y2 - mwd
-     ux2 = x2 + mwd
-     uy2 = y2 + mwd
+       lx2 = x2 - mwd
+       ly2 = y2 - mwd
+       ux2 = x2 + mwd
+       uy2 = y2 + mwd
      
       #Via
-     vx1 = x1 - vwd
-     vy1 = y1 - vwd
-     vux1 = x1 + vwd
-     vuy1 = y1 + vwd
+       vx1 = x1 - vwd
+       vy1 = y1 - vwd
+       vux1 = x1 + vwd
+       vuy1 = y1 + vwd
     
-     vx2 = x2 - vwd
-     vy2 = y2 - vwd
-     vux2 = x2 + vwd
-     vuy2 = y2 + vwd
+       vx2 = x2 - vwd
+       vy2 = y2 - vwd
+       vux2 = x2 + vwd
+       vuy2 = y2 + vwd
      
      #Metals1/2
-     path1 = [(lx1,ly1),(ux1,uy1)]
-     crd1 = coords(path1)
-     lay = 9
-     create_box(crd1,lay)
-     lay = 11
-     create_box(crd1,lay)
+       path1 = [(lx1,ly1),(ux1,uy1)]
+       crd1 = coords(path1)
+       lay = 9
+       create_box(crd1,lay)
+       lay = 11
+       create_box(crd1,lay)
      #Via
-     via1 = [(vx1,vy1),(vux1,vuy1)]
-     vcrd1 = coords(via1)
-     lay = 10
-     create_box(vcrd1,lay)
+       via1 = [(vx1,vy1),(vux1,vuy1)]
+       vcrd1 = coords(via1)
+       lay = 10
+       create_box(vcrd1,lay)
      
      #Metals1/2
-     path2 = [(lx2,ly2),(ux2,uy2)]
-     crd2 = coords(path2)
-     lay = 9
-     create_box(crd2,lay)
-     lay = 11
-     create_box(crd2,lay)
+       path2 = [(lx2,ly2),(ux2,uy2)]
+       crd2 = coords(path2)
+       lay = 9
+       create_box(crd2,lay)
+       lay = 11
+       create_box(crd2,lay)
      
      #Via
-     via2 = [(vx2,vy2),(vux2,vuy2)]
-     vcrd2 = coords(via2)
-     lay = 10
-     create_box(vcrd2,lay)
+       via2 = [(vx2,vy2),(vux2,vuy2)]
+       vcrd2 = coords(via2)
+       lay = 10
+       create_box(vcrd2,lay)
 
-#action1 = pya.Action()
-#action1.title = "Router"
-#action1.tool_tip = "Router"
-#action1.on_triggered(Router)
-#menu = pya.MainWindow.instance().menu()
+
+action1 = pya.Action()
+action1.title = "Router"
+action1.tool_tip = "Router"
+action1.on_triggered(Router)
+menu = pya.MainWindow.instance().menu()
 # Insert it main menu: where, name, action:
 #menu.insert_item("tools_menu.end", "Router", action1)
 # Insert into toolbar: for the router
-#menu.insert_item("@toolbar.end", "Router", action1)
+menu.insert_item("@toolbar.end", "Router", action1)
