b7d28ce0061232b7ccea0ad26bdb908bede1cb93
hgroller <h-groller@swbell.net>
2021-03-20 21:06:53 -0500

    Add files via upload

--- Router.py
+++ Router.py
@@ -11,6 +11,12 @@ import array
 import sys
 import math
 from importlib import reload
+from collections import OrderedDict, Counter
+
+class OrderedCounter(Counter, OrderedDict): 
+    pass
+    
+#Edit this too reflect you install path for Klayout
 sys.path.append("C:/Users/Tracy/KLayout/pymacros")
 import AstarRouter
 
@@ -54,12 +60,10 @@ while not iter.at_end():
      strlst.append(string)
      lst = [string,x,y]
      coords.append(lst)
-     #Test Print
-     #print("Text In Cell :" + iter.cell().name + ": " + string + " " +  str(x) + ","+ str(y))
   iter.next()
 
 srt = sorted(coords)
-print("The Array is: ", srt)
+#print("The Array is: ", srt)
 
 #Remove Duplicates from text list
 strlst = sorted(list(dict.fromkeys(strlst)))
@@ -76,6 +80,10 @@ def create_path(crd,wd,lay):
   layer_index = layout.layer(layer_info)
   cv.cell.shapes(layer_index).insert(Path.new(crd,wd/dbu))   
 
+def create_box(crd,lay):
+  layer_info = LayerInfo.new(lay,0)
+  layer_index = layout.layer(layer_info)
+  cv.cell.shapes(layer_index).insert(Box.new(crd[0],crd[1]))
 
 layer_info = LayerInfo.new(9,0)
 layer_index = layout.layer(layer_info) 
@@ -97,15 +105,13 @@ layer_index = layout.layer(layer_info)
 #For A* Routing 
 #This works for 2 pins only at the present time
 #All placements of Text needs to be centerd in via 
-#All placements of Text need too be on a int no floats.
-
-pathbuild = []
+#All placements of Text need too be on a int no floats
 
 for search in strlst: 
   filtered = list(filter(lambda x:x[0]==search,srt))
   print('The filtered list for is: ' , filtered[0]) 
   wd = .3
-  lay = 9
+  lay = 11
   lc1  = math.ceil(int(filtered[0][1]/1000))
   lc2  = math.ceil(int(filtered[0][2]/1000))
   lc3  = math.ceil(int(filtered[1][1]/1000))
@@ -113,12 +119,86 @@ for search in strlst:
   loc1 = (lc1,lc2)
   loc2 = (lc3,lc4)
   pth  = AstarRouter.main(loc1,loc2)
-  crd = coords(pth)
-  create_path(crd,wd,lay)
  
- 
+  #Parse in the Vert/Horz Coords
+  vert = [el[0] for el in pth]
+  horz = [el[1] for el in pth]
 
+  hoz = [k for k, v in OrderedCounter(horz).items() if v > 1]
+  vet = [k for k, v in OrderedCounter(vert).items() if v > 1]
+
+  #Build Vert/Horz Paths
+  #Metal 1
+  lay = 9
+  wd = .3
+  for p in hoz: 
+      filteredh = list(filter(lambda x:x[1]==p,pth))
+      print(filteredh)
+      crd = coords(filteredh)
+      create_path(crd,wd,lay)
+  
+  
+  wd = .3
+  for p in vet: 
+     filteredv = list(filter(lambda x:x[0]==p,pth))
+     print(filteredv)
+     crd = coords(filteredv)
+     lay = 11
+     create_path(crd,wd,lay)
+     #Build Metal1/2 Overlap
+     mwd =  wd / 2
+     vwd = wd / 4
+     x1 =  abs(int(crd[0].x)/1000)
+     y1 =  abs(int(crd[0].y)/1000)
+     x2 =  abs(int(crd[1].x)/1000)
+     y2 =  abs(int(crd[1].y)/1000)
+     #Metals
+     lx1 = x1 - mwd
+     ly1 = y1 - mwd
+     ux1 = x1 + mwd
+     uy1 = y1 + mwd
     
-
+     lx2 = x2 - mwd
+     ly2 = y2 - mwd
+     ux2 = x2 + mwd
+     uy2 = y2 + mwd
+     
+      #Via
+     vx1 = x1 - vwd
+     vy1 = y1 - vwd
+     vux1 = x1 + vwd
+     vuy1 = y1 + vwd
+    
+     vx2 = x2 - vwd
+     vy2 = y2 - vwd
+     vux2 = x2 + vwd
+     vuy2 = y2 + vwd
+     
+     #Metals1/2
+     path1 = [(lx1,ly1),(ux1,uy1)]
+     crd1 = coords(path1)
+     lay = 9
+     create_box(crd1,lay)
+     lay = 11
+     create_box(crd1,lay)
+     #Via
+     via1 = [(vx1,vy1),(vux1,vuy1)]
+     vcrd1 = coords(via1)
+     lay = 10
+     create_box(vcrd1,lay)
+     
+     #Metals1/2
+     path2 = [(lx2,ly2),(ux2,uy2)]
+     crd2 = coords(path2)
+     lay = 9
+     create_box(crd2,lay)
+     lay = 11
+     create_box(crd2,lay)
+     
+     #Via
+     via2 = [(vx2,vy2),(vux2,vuy2)]
+     vcrd2 = coords(via2)
+     lay = 10
+     create_box(vcrd2,lay)
 
 
