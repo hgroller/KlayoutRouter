3141ae458f0c84f6e166fdc111c5aa79def875ac
hgroller <h-groller@swbell.net>
2021-03-23 23:32:23 -0500

    Add files via upload
    Fix to route off grid

--- Router.py
+++ Router.py
@@ -7,11 +7,14 @@
 #          Add Maze array definition based on the Border Layer
 #    
 import pya
+import numpy as np
 import array
 import sys
 import math
+import itertools
 from importlib import reload
 from collections import OrderedDict, Counter
+import itertools
 
 class OrderedCounter(Counter, OrderedDict): 
     pass
@@ -66,7 +69,7 @@ def Get_Text():
     iter.next()
 
   srt = sorted(coords)
-  #print("The Array is: ", srt)
+  print("The Array is: ", srt)
  #Remove Duplicates from text list
   strlst = sorted(list(dict.fromkeys(strlst)))
   return strlst,srt
@@ -88,14 +91,12 @@ def create_box(crd,lay):
   layer_index = layout.layer(layer_info)
   cv.cell.shapes(layer_index).insert(Box.new(crd[0],crd[1]))
 
-layer_info = LayerInfo.new(9,0)
-layer_index = layout.layer(layer_info) 
-
-
 #Filter the list to Search the text list
 #For A* Routing 
 #This works for 2 pins only at the present time
-#All placements of Text need too be on a int no floats
+#ie: Pin A/Pin A.
+#Placements of text can be anywhere
+
 def Router():
 
   stlst,srt = Get_Text()
@@ -105,14 +106,35 @@ def Router():
     #print('The filtered list for is: ' , filtered[0]) 
     wd = .3
     lay = 11
-    lc1  = math.ceil(int(filtered[0][1]/1000))
-    lc2  = math.ceil(int(filtered[0][2]/1000))
-    lc3  = math.ceil(int(filtered[1][1]/1000))
-    lc4  = math.ceil(int(filtered[1][2]/1000))
+    #Testing
+     
+    l1  = filtered[0][1]/1000
+    l2  = filtered[0][2]/1000
+    
+    l3  = filtered[1][1]/1000
+    l4  = filtered[1][2]/1000
+    
+    #Get Decimal from float
+    x1a = l1 - int(l1)
+    y1a = l2 - int(l2)
+    x2a = l3 - int(l3)
+    y2a = l4 - int(l4)
+   
+    lc1  = int(filtered[0][1]/1000)
+    lc2  = int(filtered[0][2]/1000)
+    lc3  = int(filtered[1][1]/1000)
+    lc4  = int(filtered[1][2]/1000)
+   
     loc1 = (lc1,lc2)
     loc2 = (lc3,lc4)
+    
+    print(l1,l2,l3,l4)
+    #print(lc1,lc2,lc3,lc4)
+
+    
     pth  = AstarRouter.main(loc1,loc2)
- 
+    print("The path is = ", pth)
+    
   #Parse in the Vert/Horz Coords
     vert = [el[0] for el in pth]
     horz = [el[1] for el in pth]
@@ -120,81 +142,178 @@ def Router():
     hoz = [k for k, v in OrderedCounter(horz).items() if v > 1]
     vet = [k for k, v in OrderedCounter(vert).items() if v > 1]
 
-  #Build Vert/Horz Paths
-  #Metal 1
+   
+    #Build the Route Point to Point
+    cnt =1
     lay = 9
     wd = .3
-    for p in hoz: 
-        filteredh = list(filter(lambda x:x[1]==p,pth))
-        print(filteredh)
-        crd = coords(filteredh)
+    hlen = len(hoz)
+    for p in hoz:
+      filteredh = list(filter(lambda x:x[1]==p,pth))
+      if cnt != hlen:
+       
+        M = [x1a,y1a]
+        hout = [[i+j for i,j in zip(subl, subm)] for subl, subm in zip(filteredh, itertools.cycle([M]))]
+        crd = coords(hout)
         create_path(crd,wd,lay)
+        cnt = cnt + 1
+      else:
+         M = [x1a,y1a]
+         hout = [[i+j for i,j in zip(subl, subm)] for subl, subm in zip(filteredh, itertools.cycle([M]))]
+         xx = x2a - x1a
+         xxx = hout[1][0] + xx
+         hout[1][0] = xxx
+         yy = y2a - y1a
+         yyy = hout[1][1] + yy
+         hout[0][1] = yyy
+         hout[1][1] = yyy
+         crd = coords(hout)
+         create_path(crd,wd,lay)
+        
   
-  
+    cnt = 1
     wd = .3
-    for p in vet: 
-       filteredv = list(filter(lambda x:x[0]==p,pth))
-       print(filteredv)
-       crd = coords(filteredv)
-       lay = 11
-       create_path(crd,wd,lay)
-     #Build Metal1/2 Overlap
-       mwd =  wd / 2
-       vwd = wd / 4
-       x1 =  abs(int(crd[0].x)/1000)
-       y1 =  abs(int(crd[0].y)/1000)
-       x2 =  abs(int(crd[1].x)/1000)
-       y2 =  abs(int(crd[1].y)/1000)
-     #Metals
-       lx1 = x1 - mwd
-       ly1 = y1 - mwd
-       ux1 = x1 + mwd
-       uy1 = y1 + mwd
+    vlen = len(vet)
     
-       lx2 = x2 - mwd
-       ly2 = y2 - mwd
-       ux2 = x2 + mwd
-       uy2 = y2 + mwd
+    print("Length of vet=", vlen)
+    for p in vet: 
+      filteredv = list(filter(lambda x:x[0]==p,pth))
+      if cnt != vlen:
+          print("Vert =   ",filteredv)
+          M = [x1a,y1a]
+          vout = [[i+j for i,j in zip(subl, subm)] for subl, subm in zip(filteredv, itertools.cycle([M]))]
+          crd = coords(vout)
+          lay = 11
+          create_path(crd,wd,lay)
+          cnt = cnt + 1
+          
+     #Build Metal1/2 Overlap
+          mwd =  wd / 2
+          vwd = wd / 4
+          x1 =  abs(int(crd[0].x)/1000)
+          y1 =  abs(int(crd[0].y)/1000)
+          x2 =  abs(int(crd[1].x)/1000)
+          y2 =  abs(int(crd[1].y)/1000)
+     #Metals
+          lx1 = x1 - mwd
+          ly1 = y1 - mwd
+          ux1 = x1 + mwd
+          uy1 = y1 + mwd
+    
+          lx2 = x2 - mwd
+          ly2 = y2 - mwd
+          ux2 = x2 + mwd
+          uy2 = y2 + mwd
      
       #Via
-       vx1 = x1 - vwd
-       vy1 = y1 - vwd
-       vux1 = x1 + vwd
-       vuy1 = y1 + vwd
+          vx1 = x1 - vwd
+          vy1 = y1 - vwd
+          vux1 = x1 + vwd
+          vuy1 = y1 + vwd
     
-       vx2 = x2 - vwd
-       vy2 = y2 - vwd
-       vux2 = x2 + vwd
-       vuy2 = y2 + vwd
+          vx2 = x2 - vwd
+          vy2 = y2 - vwd
+          vux2 = x2 + vwd
+          vuy2 = y2 + vwd
      
      #Metals1/2
-       path1 = [(lx1,ly1),(ux1,uy1)]
-       crd1 = coords(path1)
-       lay = 9
-       create_box(crd1,lay)
-       lay = 11
-       create_box(crd1,lay)
+          path1 = [(lx1,ly1),(ux1,uy1)]
+       #M = [x1a,y1a]
+      #out = [[i+j for i,j in zip(subl, subm)] for subl, subm in zip(path1, itertools.cycle([M]))]
+          crd1 = coords(path1)
+          lay = 9
+          create_box(crd1,lay)
+          lay = 11
+          create_box(crd1,lay)
      #Via
-       via1 = [(vx1,vy1),(vux1,vuy1)]
-       vcrd1 = coords(via1)
-       lay = 10
-       create_box(vcrd1,lay)
+          via1 = [(vx1,vy1),(vux1,vuy1)]
+          vcrd1 = coords(via1)
+          lay = 10
+          create_box(vcrd1,lay)
      
      #Metals1/2
-       path2 = [(lx2,ly2),(ux2,uy2)]
-       crd2 = coords(path2)
-       lay = 9
-       create_box(crd2,lay)
-       lay = 11
-       create_box(crd2,lay)
+          path2 = [(lx2,ly2),(ux2,uy2)]
+          crd2 = coords(path2)
+          lay = 9
+          create_box(crd2,lay)
+          lay = 11
+          create_box(crd2,lay)
      
      #Via
-       via2 = [(vx2,vy2),(vux2,vuy2)]
-       vcrd2 = coords(via2)
-       lay = 10
-       create_box(vcrd2,lay)
-
-
+          via2 = [(vx2,vy2),(vux2,vuy2)]
+          vcrd2 = coords(via2)
+          lay = 10
+          create_box(vcrd2,lay)
+      else:
+          M = [x1a,y1a]
+          vout = [[i+j for i,j in zip(subl, subm)] for subl, subm in zip(filteredv, itertools.cycle([M]))]
+          vlen =  len(vout)
+          xx = y2a - y1a
+          xxx = vout[1][1] + xx
+          vout[1][1] = xxx
+          crd = coords(vout)
+          lay = 11
+          create_path(crd,wd,lay)
+     #Build Metal1/2 Overlap
+          mwd =  wd / 2
+          vwd = wd / 4
+          x1 =  abs(int(crd[0].x)/1000)
+          y1 =  abs(int(crd[0].y)/1000)
+          x2 =  abs(int(crd[1].x)/1000)
+          y2 =  abs(int(crd[1].y)/1000)
+     #Metals
+          lx1 = x1 - mwd
+          ly1 = y1 - mwd
+          ux1 = x1 + mwd
+          uy1 = y1 + mwd
+    
+          lx2 = x2 - mwd
+          ly2 = y2 - mwd
+          ux2 = x2 + mwd
+          uy2 = y2 + mwd
+     
+      #Via
+          vx1 = x1 - vwd
+          vy1 = y1 - vwd
+          vux1 = x1 + vwd
+          vuy1 = y1 + vwd
+    
+          vx2 = x2 - vwd
+          vy2 = y2 - vwd
+          vux2 = x2 + vwd
+          vuy2 = y2 + vwd
+     
+     #Metals1/2
+          path1 = [(lx1,ly1),(ux1,uy1)]
+       #M = [x1a,y1a]
+      #out = [[i+j for i,j in zip(subl, subm)] for subl, subm in zip(path1, itertools.cycle([M]))]
+          crd1 = coords(path1)
+          lay = 9
+          create_box(crd1,lay)
+          lay = 11
+          create_box(crd1,lay)
+     #Via
+          via1 = [(vx1,vy1),(vux1,vuy1)]
+          vcrd1 = coords(via1)
+          lay = 10
+          create_box(vcrd1,lay)
+     
+     #Metals1/2
+          path2 = [(lx2,ly2),(ux2,uy2)]
+          crd2 = coords(path2)
+          lay = 9
+          create_box(crd2,lay)
+          lay = 11
+          create_box(crd2,lay)
+     
+     #Via
+          via2 = [(vx2,vy2),(vux2,vuy2)]
+          vcrd2 = coords(via2)
+          lay = 10
+          create_box(vcrd2,lay)
+      
+      
+      
 action1 = pya.Action()
 action1.title = "Router"
 action1.tool_tip = "Router"
@@ -204,3 +323,17 @@ menu = pya.MainWindow.instance().menu()
 #menu.insert_item("tools_menu.end", "Router", action1)
 # Insert into toolbar: for the router
 menu.insert_item("@toolbar.end", "Router", action1)
+
+
+#Build Matrix for later
+#Build Maze based on  Border Boundary Box
+#rows = int(input()) ux from Border Box
+#cols = int(input())  uy from Border Box
+
+#rows = round(abs(int(ux))/1000)
+#cols =  round(abs(int(uy))/1000)
+
+#w, h = rows, cols;
+#Matrix = [[0 for x in range(w)] for y in range(h)] 
+#for  s in Matrix:
+    #print(s)
